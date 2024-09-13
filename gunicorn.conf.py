83ccd88657c3436c2d366ff611a3285350f62ced
Alexey Leeper <alexeyleeper@gmail.com>
2024-09-12 18:14:02 -0700

    modified ip address in gunicorn config file to take environment variable

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,20 +1,8 @@
-import socket
-import fcntl
-import struct
+import os
 import multiprocessing
 
-def get_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x89115,
-        struct.pack('256s', bytes(ifname[:15], 'utf-8'))
-    )[20: 24])
-
-ip_addr = get_ip_address('eth0')
-
-bind = f"{ip_addr}:8000"
+# Use environment variables for binding
+bind = f"0.0.0.0:{os.getenv('PORT', '8000')}"
 
 workers = multiprocessing.cpu_count() * 2 + 1
-
 preload = True
